3ea3e9d2c02bbb16b788de60fcb9332495ae5839
mehernosh <mehernosh@users.noreply.github.com>
2020-03-12 13:41:24 +0530

    renamed function

--- src/plugin/prodsafe.plugin.zsh
+++ src/plugin/prodsafe.plugin.zsh
@@ -8,17 +8,20 @@ function tab-reset() {
     NAME="Default"
     echo -e "\033]6;1;bg;*;default\a"    
     echo -e "\033]50;SetProfile=$NAME\a"
+    # export DISABLE_AUTO_TITLE="false"
 }
 
-function colorprodheroku() {
+function prodsafeheroku() {
     if [[ -n "$ITERM_SESSION_ID" ]]; then
         trap "tab-reset" INT EXIT
         if [[ "$*" =~ ".*prod.*" ]]; then
             prodtab prodsafe
         fi
+        # DISABLE_AUTO_TITLE="false"
+        # echo -ne "\033]0;heroku "$*"\007"
     fi
     heroku $*
 }
 compdef _heroku prodtab=heroku
 
-alias heroku="colorprodheroku"
+alias heroku="prodsafeheroku"
